713f57a4e09a53bbab33a61bf5bdde42b23c13d1
Peter Mgembe <peterrobsson@gmail.com>
2023-11-09 14:06:31 +0300

    solved question 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 """ The module that will contain session authentication """
 from api.v1.auth.auth import Auth
+from flask import session
+import uuid
 
 
 class SessionAuth(Auth):
     """ The class for session authentication """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ The function for creating a session """
+        if user_id is None:
+            return None
+        elif not isinstance(user_id, str):
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
