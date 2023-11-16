e74fa9730aea09db6e0e3b28541d0b87aab7eb66
Peter Mgembe <peterrobsson@gmail.com>
2023-11-16 16:28:02 +0300

    solved question 19 again

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -103,7 +103,7 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route('reset_password', methods=['PUT'])
+@app.route('/reset_password', methods=['PUT'])
 def update_password():
     """ The function to update the password """
     email = request.form.get('email')
